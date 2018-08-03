cde084227fd4e57053947b79fe7c9812ab6eb0be
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-03 14:45:12 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,8 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def initialize(options = {})
+    options.each { |property, value| self.send("#{property}=", value)}
+  end 
 end
